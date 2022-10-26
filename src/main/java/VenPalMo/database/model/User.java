9354b7ace0612448969d3a1e86945472860b7d65
Devin Butts <devinmbutts@gmail.com>
2022-10-26 15:41:04 -0400

    10.26

--- src/main/java/VenPalMo/database/model/User.java
+++ src/main/java/VenPalMo/database/model/User.java
@@ -27,13 +27,8 @@ public class User {
     @Column(name = "phone_number")
     private String phoneNumber;
 
-    private String address;
-
-    private String city;
-
-    private String state;
-
-    private String zip;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
 
     @Column(name = "joined_date")
     private Date joinedDate;
@@ -52,27 +47,26 @@ public class User {
     public User() {
     }
 
-    public User(String firstName, String lastName, String email, String password, String phoneNumber, String address, String city, String state, String zip, Date joinedDate, Date birthDate, List<Account> accounts, String role) {
+    public User(String firstName, String lastName, String email, String password, String phoneNumber, Address address, Date joinedDate, Date birthDate, List<Account> accounts, String role, boolean active) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
         this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
         this.joinedDate = joinedDate;
         this.birthDate = birthDate;
         this.accounts = accounts;
         this.role = role;
+        this.active = active;
     }
-
-    //get set
-
 
     public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -115,36 +109,12 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getAddress() {
+    public Address getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(Address address) {
         this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
     }
 
     public Date getJoinedDate() {
