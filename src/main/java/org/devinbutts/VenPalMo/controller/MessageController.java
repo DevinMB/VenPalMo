19ff3dbf72679eea76f91534707250aec2d37121
Devin Butts <devinmbutts@gmail.com>
2022-12-09 15:32:13 -0500

    clean up search page css

--- src/main/java/org/devinbutts/VenPalMo/controller/MessageController.java
+++ src/main/java/org/devinbutts/VenPalMo/controller/MessageController.java
@@ -1,20 +1,14 @@
 package org.devinbutts.VenPalMo.controller;
 
-
 import org.devinbutts.VenPalMo.dao.DisplayUserDAO;
-import org.devinbutts.VenPalMo.dao.MessageDAO;
 import org.devinbutts.VenPalMo.dao.UserDAO;
-import org.devinbutts.VenPalMo.model.Message;
-import org.devinbutts.VenPalMo.model.User;
 import org.devinbutts.VenPalMo.model.dto.MessageDTO;
 import org.devinbutts.VenPalMo.model.dto.UserDTO;
 import org.devinbutts.VenPalMo.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.security.Principal;
 import java.util.List;
 
@@ -34,41 +28,30 @@ public class MessageController {
     @ResponseBody
     @GetMapping
     public List<MessageDTO> getMessages(Principal principal) {
-
         return messageService.getUserMessageDTOs(principal);
-
     }
 
     @GetMapping(value = "/{id}")
     public ModelAndView getConversation(Principal principal, @PathVariable int id){
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("message");
-
-        List<MessageDTO> messages = messageService.getUserMessageDTOs(principal,id);
-        modelAndView.addObject("messages",messages);
-
-        UserDTO loggedInUser =  displayUserDAO.findUserByEmail(principal.getName());
-        modelAndView.addObject("loggedInUser",loggedInUser);
-
-        UserDTO otherUser = displayUserDAO.findByUserId(id);
-        modelAndView.addObject("otherUser",otherUser);
-
-        modelAndView.addObject("messageDTO",new MessageDTO());
-
-        return modelAndView;
-
+        return getSearchPageModelAndView(principal, id);
     }
 
     @PostMapping(value = "/{id}")
     public ModelAndView getConversation(Principal principal, @PathVariable int id,@ModelAttribute(value="messageDTO") MessageDTO messageDTO){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("message");
         messageDTO.setMessageType("Sent");
         messageDTO.setEmail(userDAO.findByUserId(id).getEmail());
         messageService.saveMessage(messageDTO,principal);
 
-        List<MessageDTO> messages = messageService.getUserMessageDTOs(principal,id);
+        ModelAndView modelAndView = getSearchPageModelAndView(principal, id);
+
+        return modelAndView;
+    }
+
+    private ModelAndView getSearchPageModelAndView(Principal principal, int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("message");
+
+        List<MessageDTO> messages = messageService.getUserMessageDTOs(principal, id);
         modelAndView.addObject("messages",messages);
 
         UserDTO loggedInUser =  displayUserDAO.findUserByEmail(principal.getName());
@@ -78,12 +61,7 @@ public class MessageController {
         modelAndView.addObject("otherUser",otherUser);
 
         modelAndView.addObject("messageDTO",new MessageDTO());
-
         return modelAndView;
     }
-
-
-
-
 
 }
